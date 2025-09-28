143f2b85ffd48663f653332d25f23cd5da76a1c0
Nadiia Shishova <nadin71983@gmail.com>
2025-09-28 22:26:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,33 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('form');
+  let idCounter = 1;
 
   forms.forEach((form) => {
     const inputs = form.querySelectorAll('input');
 
     inputs.forEach((input) => {
-      if (input.name) {
-        // робимо "firstName" → "First name"
-        const formattedName = input.name
-          .replace(/([A-Z])/g, ' $1')
-          .replace(/^./, (str) => str.toUpperCase());
+      const formattedName = input.name
+        .replace(/[-_]/g, ' ')
+        .replace(/([A-Z])/g, ' $1')
+        .replace(/\s+/g, ' ')
+        .trim()
+        .replace(/^./, (str) => str.toUpperCase());
 
-        // додаємо placeholder
-        input.setAttribute('placeholder', formattedName);
+      input.setAttribute('placeholder', formattedName);
 
-        // додаємо label
-        if (input.id) {
-          const label = document.createElement('label');
-
-          label.classList.add('field-label');
-          label.setAttribute('for', input.id);
-          label.textContent = formattedName;
-
-          input.parentNode.insertBefore(label, input);
-        }
+      if (!input.id) {
+        input.id = `${input.name || 'input'}-${idCounter++}`;
       }
+
+      const label = document.createElement('label');
+
+      label.classList.add('field-label');
+      label.setAttribute('for', input.id);
+      label.textContent = formattedName;
+
+      input.parentNode.insertBefore(label, input);
     });
   });
 });
