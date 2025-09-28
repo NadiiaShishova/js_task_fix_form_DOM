6e47d10e8f37743778523ac9acf93db35bd867ac
Nadiia Shishova <nadin71983@gmail.com>
2025-09-28 22:15:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('form');
 
-// write code here
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      if (input.name) {
+        // робимо "firstName" → "First name"
+        const formattedName = input.name
+          .replace(/([A-Z])/g, ' $1')
+          .replace(/^./, (str) => str.toUpperCase());
+
+        // додаємо placeholder
+        input.setAttribute('placeholder', formattedName);
+
+        // додаємо label
+        if (input.id) {
+          const label = document.createElement('label');
+
+          label.classList.add('field-label');
+          label.setAttribute('for', input.id);
+          label.textContent = formattedName;
+
+          input.parentNode.insertBefore(label, input);
+        }
+      }
+    });
+  });
+});
